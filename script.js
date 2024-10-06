129184e3ba97cde51d07295e0db77f7b34c2a15a
Abay-portofolio <abayangaw29@gmail.com>
2024-10-06 08:42:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+    // Here you can add code to send the message, e.g., via an API
+});
